8c0d3c27943c14b82314fae2ed83668f9f494aa4
diegoo1367 <diegaomacena@gmail.com>
2024-03-20 09:19:19 -0300

    Alteração 20/03

---
+++ script.js
@@ -0,0 +1,27 @@
+/*
+let a = "5";
+let b = 2;
+console.log(typeof(a));
+console.log(typeof(b));
+*/
+
+//let numero1 = 0.1;
+//let numero2 = 0.2;
+
+let texto1 = "ola";
+let texto2 = "mundo";
+
+
+let resultadoTexto = texto1 + ", " + texto2 + "dkaskdaskdak" + "ifjfidsfjsd";
+
+console.log(resultadoTexto);
+
+
+/*
+let resultado = a - b;
+let resultado2 = a + b;
+
+console.log(resultado);
+console.log(resultado2)
+
+*/
