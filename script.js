794c9875e2ea7d7d0b2eabae957f9ffa43cf3802
diegoo1367 <diegaomacena@gmail.com>
2024-03-20 10:52:43 -0300

    Final aula 20/03

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
+let conteudo;
+
+conteudo = document.getElementById("titulos")
+
+conteudo.innerHTML = "<h1>Esse conteudo esta vindo do javascript<h1>"
+
+letconteudo2 = document .getElementById("paragrafo-legal")
+conteudo2.innerHTML = "<p> esse paragrafo vem do js<p>"
+
+console.log(typeof(conteudo));
+
+console.log(conteudo);
+
+
+
+
+
+
+
+
+
+//imprima os numero de 1 a 5 na tela
+//for (let i = 1; i < 100000; i += 1) {
+//     console.log(i);
+//}
+//let i = i
+//while (i < 6) {
+//    console.log(i);
+//    i++; 
+//}
 /*
 let a = "5";
 let b = 2;
 console.log(typeof(a));
 console.log(typeof(b));
-*/
 
 //let numero1 = 0.1;
 //let numero2 = 0.2;
@@ -17,11 +46,43 @@ let resultadoTexto = texto1 + ", " + texto2 + "dkaskdaskdak" + "ifjfidsfjsd";
 console.log(resultadoTexto);
 
 
-/*
 let resultado = a - b;
 let resultado2 = a + b;
 
 console.log(resultado);
 console.log(resultado2)
+
+
+
+let idade = 18;
+let nome= "joao";
+let altura = 1.80;
+
+
+if(idade >= 18 || nome != "pedro") {
+    console.log("ola ")
+}
+
+
+let meuArray = [1, 3 , 4, 6, 0, -3];
+console.log(meuArray[0]);
+console.log(meuArray);
+
+console.log(meuArray.length);
+
+let pessoa = {
+    nome:"joao",
+    idade: 18,
+    altura: 1.78
+
+}
+
+console.log("a idade da pessoa" + pessoa.nome + "é" +pessoa.idade);
+
+let nome = "joao";
+
+if (nome == "joao") {
+    console.log("Oi, joao!");
+}
 
 */
