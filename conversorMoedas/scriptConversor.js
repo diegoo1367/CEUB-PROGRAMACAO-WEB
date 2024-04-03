bb6340edeecd739e488b680a210fbb9b494d33aa
diegoo1367 <diegaomacena@gmail.com>
2024-04-03 10:50:47 -0300

    Alteração aula 03/04

--- conversorMoedas/scriptConversor.js
+++ conversorMoedas/scriptConversor.js
@@ -2,23 +2,63 @@ const valoresConversao = {
     real: {
         euro: 0.19,
         dolar: 0.20,
-        simbolo: "R$"
+        simbolo:"R$"
     },
     dolar:{
         real: 4.99,
         euro: 0.92,
-        simbolo: "US$"
+        simbolo:"US$"
     },
     euro: {
         real:5.40,
         dolar:1.08,
-        simbolo: "EU"
+        simbolo:"EU"
     }   
 
 }
 
+const botaoInverter = document.getElementById("botao-inverter");
+botaoInverter.addEventListener("click", inverter);
+
+const botaoConverter = document.getElementById("botao-converter")
+botaoConverter.addEventListener("click", converter)
+
+const botaoLimpar = document.getElementById("botao-limpar")
+botaoLimpar.addEventListener("click", limpar)
+
+document.addEventListener("keydown", function(event){
+    console.log(event)
+})
+
+let valorUsuario = document.getElementById("valorEntrada");
+valorUsuario.addEventListener("keypress", function(event) {
+
+    event.preventDefault();
+    console.log(event);
+
+    if (event.ctrkey == true && event.code =="Keyl") {
+        event.preventDefault();
+        limpar();
+    }
+
+    if(event.ctrkey == true && event.key == "KeyI") {
+        inverter();
+    }
+
+    if (event.key == true && event.key == "Enter") {
+        event.preventDefault();
+        converter();
+    }
+});
+
+
 function converter() {
         let valorUsuario = document.getElementById("valorEntrada").value;
+
+        if(valorUsuario <= 0 || valorUsuario == "") {
+            alert("Verificar valor ");
+            return;
+        }
 
         let moeda1 = document.getElementById("moeda1").value;
         let moeda2 = document.getElementById("moeda2").value;
@@ -26,17 +66,20 @@ function converter() {
         if(moeda1 == moeda2) {
             alert("As moedas são iguais!!!")
             return;
-
         }
 
-        let simbolo = "";
-      
+        let simbolo = valoresConversao [moeda2]["simbolo"];
+        //console.log(simbolo);
 
+        
         let resultado = valorUsuario * valoresConversao[moeda1][moeda2];
-    
+
+
+
         let paragrafoResultado = document.getElementById("resultado");
-        paragrafoResultado.textContent = resultado;
-    
+        paragrafoResultado.textContent = simbolo + " " + resultado.toFixed(2);
+        
+
 }
 
 function limpar() {
@@ -45,6 +88,7 @@ function limpar() {
 
     valorEntrada = document.getElementById("valorEntrada")
     valorEntrada.value = "";
+
 }
 
 function inverter() {
